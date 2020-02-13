49db3a9900bca8a533617aeebd1af5794002dc19
ぐらにゅ <42028429+guranytou@users.noreply.github.com>
2020-02-13 21:35:20 +0900

    変数名が気になったので直し

--- battlefieldRun.js
+++ battlefieldRun.js
@@ -6,8 +6,8 @@
      },
        computed: {
          output: function(){
-           const HELL95 = 51500 * 2 + 910000;
-           const HELL100 = 51500 * 4 + 2680000;
+           const Hell95 = 51500 * 2 + 910000;
+           const Hell100 = 51500 * 4 + 2680000;
 
            return  Math.floor((this.input / 2 / HELL95 * 10) +  (this.input / 2 / HELL100 * 20))
          },
