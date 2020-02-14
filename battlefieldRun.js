dd147484440dffe98e5f1bf557bc7e5fc7f963d2
ぐらにゅ <42028429+guranytou@users.noreply.github.com>
2020-02-14 21:52:23 +0900

    ラジオボタン追加

--- battlefieldRun.js
+++ battlefieldRun.js
@@ -9,7 +9,7 @@
            const Hell95 = 51500 * 2 + 910000;
            const Hell100 = 51500 * 4 + 2680000;
 
-           return  Math.floor((this.input / 2 / HELL95 * 10) +  (this.input / 2 / HELL100 * 20))
+           return  Math.floor((this.input / 2 / Hell95 * 10) +  (this.input / 2 / Hell100 * 20))
          },
        },
    })
