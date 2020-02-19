b1541c7d940a7a6e0591e956216845b6a07ca2cc
ぐらにゅ <42028429+guranytou@users.noreply.github.com>
2020-02-19 12:04:24 +0900

    条件分岐をつける

--- battlefieldRun.js
+++ battlefieldRun.js
@@ -2,15 +2,32 @@
    var triger = new Vue({
      el: '#app',
      data: {
-       input: '0'
+       input: '0',
+       output: '0',
+       trigerDigest: 'false'
      },
-       computed: {
-         output: function(){
-           const Hell95 = 51500 * 2 + 910000;
-           const Hell100 = 51500 * 4 + 2680000;
+     methods: {
+       changeCheckbox: function (event) {
+         const Hell95Contribution = 51500 * 2 + 910000;
+         const Hell100Contribution = 51500 * 4 + 2680000;
 
-           return  Math.floor((this.input / 2 / Hell95 * 10) +  (this.input / 2 / Hell100 * 20))
-         },
-       },
+         if(this.trigerDigest === 'Hell95'){
+           // ŒvŽZŽ®
+           // Math.floor(this.input / Hell95Contribution * 10);
+           console.log(this.trigerDigest);
+         }else if(this.trigerDigest === 'Hell100'){
+           // ŒvŽZŽ®
+           // Math.floor(this.input / Hell100Contribution * 20);
+           console.log(this.trigerDigest);
+         }else if(this.trigerDigest === 'both'){
+           // ŒvŽZŽ®
+           // Math.floor((this.input / 2 / Hell95Contribution * 10) +  (this.input / 2 / Hell100Contribution * 20));
+           console.log(this.trigerDigest);
+         }else{
+           //Žb’è
+           this.output = 0;
+         }
+       }
+     }
    })
 }());
