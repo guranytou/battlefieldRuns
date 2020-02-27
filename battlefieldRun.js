014a3e5c42e191550ce10cecfb018fccc147c019
ぐらにゅ <42028429+guranytou@users.noreply.github.com>
2020-02-27 16:59:27 +0900

    完成

--- battlefieldRun.js
+++ battlefieldRun.js
@@ -2,32 +2,28 @@
    var triger = new Vue({
      el: '#app',
      data: {
-       input: '0',
-       output: '0',
+       input: 0,
        trigerDigest: 'false'
      },
-     methods: {
-       changeCheckbox: function (event) {
-         const Hell95Contribution = 51500 * 2 + 910000;
-         const Hell100Contribution = 51500 * 4 + 2680000;
+     computed: {
+       output: function(){
+           const Hell95Contribution = 51500 * 2 + 910000;
+           const Hell100Contribution = 51500 * 4 + 2680000;
 
-         if(this.trigerDigest === 'Hell95'){
-           // ŒvŽZŽ®
-           // Math.floor(this.input / Hell95Contribution * 10);
-           console.log(this.trigerDigest);
-         }else if(this.trigerDigest === 'Hell100'){
-           // ŒvŽZŽ®
-           // Math.floor(this.input / Hell100Contribution * 20);
-           console.log(this.trigerDigest);
-         }else if(this.trigerDigest === 'both'){
-           // ŒvŽZŽ®
-           // Math.floor((this.input / 2 / Hell95Contribution * 10) +  (this.input / 2 / Hell100Contribution * 20));
-           console.log(this.trigerDigest);
-         }else{
-           //Žb’è
-           this.output = 0;
+           if(this.trigerDigest === 'Hell95'){
+            // ŒvŽZŽ®
+             return Math.floor(this.input / Hell95Contribution * 10)
+           }else if(this.trigerDigest === 'Hell100'){
+            // ŒvŽZŽ®
+             return Math.floor(this.input / Hell100Contribution * 20)
+           }else if(this.trigerDigest === 'both'){
+            // ŒvŽZŽ®
+             return Math.floor((this.input / 2 / Hell95Contribution * 10) +  (this.input / 2 / Hell100Contribution * 20))
+           }else{
+             //Žb’è
+             return 0;
+            }
          }
-       }
      }
    })
 }());
