c8bc7b67af116c1095a2e9cb730baa2bca770387
ぐらにゅ <42028429+guranytou@users.noreply.github.com>
2020-02-27 20:06:19 +0900

    150HELL追加

--- battlefieldRun.js
+++ battlefieldRun.js
@@ -9,19 +9,23 @@
        output: function(){
            const Hell95NeedTriger = 10;
            const Hell100NeedTriger = 20;
+           const Hell150NeedTriger = 20;
            const ExTriger = 4;
            const Hell95Contribution = 51500 * (Hell95NeedTriger / ExTriger) + 910000;
            const Hell100Contribution = 51500 * (Hell100NeedTriger / ExTriger) + 2680000;
+           const Hell150Contribution = 51500 * (Hell150NeedTriger / ExTriger) + 3600000;
 
            if(this.trigerDigest === 'Hell95'){
             // ŒvŽZŽ®
-             return Math.floor(this.input / Hell95Contribution * 10)
+             return Math.floor(this.input / Hell95Contribution * Hell95NeedTriger)
            }else if(this.trigerDigest === 'Hell100'){
             // ŒvŽZŽ®
-             return Math.floor(this.input / Hell100Contribution * 20)
+             return Math.floor(this.input / Hell100Contribution * Hell100NeedTriger)
+           }else if(this.trigerDigest === 'Hell150'){
+              return Math.floor(this.input / Hell150Contribution * Hell150NeedTriger)
            }else if(this.trigerDigest === 'both'){
             // ŒvŽZŽ®
-             return Math.floor((this.input / 2 / Hell95Contribution * 10) +  (this.input / 2 / Hell100Contribution * 20))
+             return Math.floor((this.input / 2 / Hell95Contribution * Hell95NeedTriger) +  (this.input / 2 / Hell100Contribution * Hell100NeedTriger))
            }else{
              //Žb’è
              return 0;
